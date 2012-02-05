e5ca3f35c22e43d2322119ba1c89477873b562af
Odd Stråbø <oddstr13@openshell.no>
2012-02-05 18:38:50 +0100

    setting config values to something that fits my small server better

--- WebInterface/scripts/config.php
+++ WebInterface/scripts/config.php
@@ -6,12 +6,12 @@
 $db_host		= 'mywebsite.com'; //change these!!
 $db_user		= 'username';//change these!!
 $db_pass		= 'password1234';//change these!!
-$db_database	= 'minecraft'; //change these!!
+$db_database		= 'minecraft'; //change these!!
 
 /* Market Price config */
 
-$maxSellPrice = 10000; //this is per item
-$marketDaysMin	= 30; //number of past days to take the average of the sales for to work out market price (bigger number for smaller servers)
+$maxSellPrice = 1000; //this is per item
+$marketDaysMin	= 120; //number of past days to take the average of the sales for to work out market price (bigger number for smaller servers)
 
 /* Design config */
 
@@ -20,14 +20,14 @@ $cssFile = "main"; //will be collecting a list of cool css files, but "main" is 
 
 /* Auction fees and config */
 
-$auctionLength = 10; //days before auction is ended, and items returned to owner
-$auctionFee = 1; //% of the market price you are charged to auction the item
-$chargeAdmins = false; //whether web admins get charged fees
+$auctionLength = 120; //days before auction is ended, and items returned to owner
+$auctionFee = 0.0001; //% of the market price you are charged to auction the item
+$chargeAdmins = true; //whether web admins get charged fees
 
 /* Storage config */
 
-$costPerItemPerDay = 0.20; //cost to store 1 item for a day, per item not per stack eg. 64 x Wool + 10 x Stone = 14.8 per day when cost = 0.20
-$numberOfChecksPerDay = 4; //checks take a while, for whoever happens to open the page when a check is needed, but more checks make it harder to avoid the cost.
+$costPerItemPerDay = 0; //cost to store 1 item for a day, per item not per stack eg. 64 x Wool + 10 x Stone = 14.8 per day when cost = 0.20
+$numberOfChecksPerDay = 1; //checks take a while, for whoever happens to open the page when a check is needed, but more checks make it harder to avoid the cost.
 
 /* Currency config */
 
@@ -36,7 +36,7 @@ $currencyPostfix = ""; //appears after the cost values eg. "Pounds" would make 1
 
 /* iConomy config */
 
-$useMySQLiConomy = false; //you you have iConomy data in another table in the same database?
+$useMySQLiConomy = true; //you you have iConomy data in another table in the same database?
 $iConTableName = "iConomy"; //"iConomy" is the default table name when using MySQL with iConomy
 
 /* Mail config */
