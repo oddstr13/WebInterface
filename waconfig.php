010fbf01b6aa761355c5c55e7c4c2983c7b1153c
Odd Stråbø <oddstr13@openshell.no>
2012-02-05 19:12:49 +0100

    added example waconfig.php

---
+++ waconfig.php
@@ -0,0 +1,12 @@
+<?php
+    $db_host        = "localhost";
+    $db_user        = "minecraft";
+    $db_pass        = "changeme";
+    $db_database    = "minecraft";
+
+    $osweb_db_host     = "localhost";
+    $osweb_db_user     = "mcweb";
+    $osweb_db_password = "changeme";
+    $osweb_db_database = "minecraft";
+    $osweb_db_prefix   = "osweb";
+?>
